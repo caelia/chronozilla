897dc32c5d6bbe860f63cb0414f91b126fde4634
Matt Gushee <matt@gushee.net>
2012-09-06 09:35:48 -0600

    Created project.

---
+++ chronozilla.scm
@@ -0,0 +1,25 @@
+;;; chronozilla.scm -- Chronozilla is a high-level library for modeling [human] events
+;;;   Copyright © 2012 by Matthew C. Gushee. This is open-source software released
+;;;   under the BSD license. See the LICENSE file for details.
+
+
+(module chronozilla *
+
+;;; [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[
+;;; --  DATE-TIME EXPRESSIONS  -----------------------------------------
+;;; A date-time expression can be absolute or relative, and definite or
+;;; indefinite.
+
+(define (make-absolute-dt . seconds)
+
+
+
+
+;;; ]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]
+)
+
+
+;;; [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[
+;;; --------------------------------------------------------------------
+;;; ====================================================================
+;;; ]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]
